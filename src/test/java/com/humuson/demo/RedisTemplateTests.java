2e9f09e17f6cd7b788f6c49dbd3abf0ec2fa5110
kbs <vlakd0711@naver.com>
2019-11-04 11:08:39 +0900

    add test code

--- src/test/java/com/humuson/demo/RedisTemplateTests.java
+++ src/test/java/com/humuson/demo/RedisTemplateTests.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,13 +48,14 @@ public class RedisTemplateTests {
 		setupMap.put(m3.getId(), m3);
 	}
 	
+	@AfterEach
+	public void tearDown() {
+		redisTemplate.opsForHash().delete("kbs", m1.getId(), m2.getId(), m3.getId());
+	}
+	
 	@Test
 	@DisplayName("redisTemplate을 활용한 삽입 테스트")
-	public void test() {
-//		redisTemplate.opsForHash().put("kbs", m1.getId(), m1);
-//		redisTemplate.opsForHash().put("kbs", m2.getId(), m2);
-//		redisTemplate.opsForHash().put("kbs", m3.getId(), m3);
-		
+	public void testInsertWithRedisTemplate() {
 		// arrange
 		redisTemplate.opsForHash().putAll("kbs", setupMap);
 		
@@ -61,5 +64,44 @@ public class RedisTemplateTests {
 		
 		// assert
 		assertThat(lists).hasSize(3).contains(m1, m2, m3);
+	}
+	
+	@Test
+	public void testRedisTemplateEntries() {
+		// arrange
+		redisTemplate.opsForHash().putAll("kbs", setupMap);
+		
+		// act
+		Map<Object, Object> entries = redisTemplate.opsForHash().entries("kbs");
+		
+		// assert
+		assertThat(entries).isNotEmpty().hasSize(3).containsKeys(m1.getId(), m2.getId(), m3.getId());
+	}
+	
+	@Test
+	public void testUpdateWithRedisTemplate() {
+		// arrange
+		redisTemplate.opsForHash().putAll("kbs", setupMap);
+		
+		// act
+		m3.setMessage("Updated message");
+		redisTemplate.opsForHash().put("kbs", m3.getId(), m3);
+		
+		// assert
+		MessageModel updated = (MessageModel) redisTemplate.opsForHash().get("kbs", m3.getId());
+		Assertions.assertEquals(m3.getMessage(), updated.getMessage());
+	}
+	
+	@Test
+	public void testDeleteWithRedisTemplate() {
+		// arrange
+		redisTemplate.opsForHash().putAll("kbs", setupMap);
+		
+		// act
+		redisTemplate.opsForHash().delete("kbs", m2.getId());
+		
+		// assert
+		Map<Object, Object> entries = redisTemplate.opsForHash().entries("kbs");
+		assertThat(entries).isNotEmpty().hasSize(2).containsKeys(m1.getId(), m3.getId()).doesNotContainKey(m2.getId());
 	}
 }
